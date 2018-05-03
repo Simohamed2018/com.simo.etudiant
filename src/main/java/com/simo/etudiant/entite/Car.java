87ed816f25e2a0a31fc3fad80f7bebf8bf9f0030
Simohamed <simo1408@gmail.com>
2018-05-03 18:04:33 -0400

    adda car class

---
+++ src/main/java/com/simo/etudiant/entite/Car.java
@@ -0,0 +1,23 @@
+package com.simo.etudiant.entite;
+
+/**
+ * Created by Simohamed on 2018-05-03.
+ */
+public class Car{
+private String modele;
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public Car(String modele) {
+        this.modele = modele;
+    }
+
+    public Car() {
+    }
+}
