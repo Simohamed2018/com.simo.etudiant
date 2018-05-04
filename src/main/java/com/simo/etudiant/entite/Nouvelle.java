9b0b6abf5dc159c2589b3065dd9c563e4a09bf14
Simohamed <simo1408@gmail.com>
2018-05-03 20:27:05 -0400

    nouvelle

--- src/main/java/com/simo/etudiant/entite/Nouvelle.java
+++ src/main/java/com/simo/etudiant/entite/Nouvelle.java
@@ -4,4 +4,26 @@ package com.simo.etudiant.entite;
  * Created by Simohamed on 2018-05-03.
  */
 public class Nouvelle {
+
+    //coomentaire
+    private String place;
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    @Override
+    public String toString() {
+        return "Nouvelle{" +
+                "place='" + place + '\'' +
+                '}';
+    }
+
+    public Nouvelle(String place) {
+        this.place = place;
+    }
 }
